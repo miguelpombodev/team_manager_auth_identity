5d5d7651fe524721d0e88c62e92bab95150cf766
Miguel Pombo <miguel.pomboshape@modec.com>
2025-10-09 00:33:47 -0300

    feat: implement Serilog

--- src/TeamManager.API/Program.cs
+++ src/TeamManager.API/Program.cs
@@ -1,18 +1,30 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Serilog;
+using TeamManager.API.Extensions;
 using TeamManager.Domain.Entities;
 using TeamManager.Infrastructure.Extensions;
 using TeamManager.Infrastructure.Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddOpenApi();
+var configuration = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .AddJsonFile(
+        $"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}.json",
+        optional: true, reloadOnChange: true).AddEnvironmentVariables().AddCommandLine(args).Build();
+
+builder.Host.ConfigureSerilog();
 
 builder.Services
-    .AddInfrastructureServices()
-    .AddIdentitySetup();
+    .AddOpenApi()
+    .AddInfrastructureServices(configuration)
+    .AddIdentitySetup()
+    .AddAuthenticationAndAuthorization();
 
 var app = builder.Build();
+
+app.AddSerilog();
 
 if (app.Environment.IsDevelopment())
 {
@@ -23,16 +35,16 @@ if (app.Environment.IsDevelopment())
     var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
     dbContext.Database.Migrate();
 
-    var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+    var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole<Guid>>>();
 
     if (!await roleManager.RoleExistsAsync(Roles.TeamLeader))
     {
-        await roleManager.CreateAsync(new IdentityRole(Roles.TeamLeader));
+        await roleManager.CreateAsync(new IdentityRole<Guid>(Roles.TeamLeader));
     }
 
     if (!await roleManager.RoleExistsAsync(Roles.TeamMember))
     {
-        await roleManager.CreateAsync(new IdentityRole(Roles.TeamMember));
+        await roleManager.CreateAsync(new IdentityRole<Guid>(Roles.TeamMember));
     }
 }
 
