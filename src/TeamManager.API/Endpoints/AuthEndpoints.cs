670d9b17c2cde971143516e3c13414a76ecb94f7
Miguel Pombo <miguel.pomboshape@modec.com>
2025-10-11 11:47:26 -0300

    feat: apply more documentation to login endpoint

--- src/TeamManager.API/Endpoints/AuthEndpoints.cs
+++ src/TeamManager.API/Endpoints/AuthEndpoints.cs
@@ -3,11 +3,15 @@ using Microsoft.AspNetCore.Mvc;
 using TeamManager.Application.Abstractions.Requests.Auth;
 using TeamManager.Application.Features.Auth;
 using TeamManager.Domain.Entities;
+using TeamManager.Domain.Providers.Authentication.Entities;
 
 namespace TeamManager.API.Endpoints;
 
 public static class AuthEndpoints
 {
+    private const string LoginProvider = "TeamManager";
+    private const string TokenName = "refresh_token";
+
     public static void MapEndpoint(IEndpointRouteBuilder app)
     {
         var group = app.MapGroup("auth").WithTags("Auth");
@@ -46,11 +50,17 @@ public static class AuthEndpoints
                 await signInManager.SignInAsync(result.Data.Item2, isPersistent: true);
                 await userManager.SetAuthenticationTokenAsync(
                     result.Data.Item2,
-                    "TeamManager",
-                    "refresh_token",
+                    LoginProvider,
+                    TokenName,
                     result.Data.Item1.RefreshToken);
 
                 return Results.Ok(result.Data.Item1);
-            });
+            })
+            .WithSummary("Login Member and return a cookie")
+            .WithDescription(
+                "Tries to login a possible member, if successful return a token access and a refresh token, and also set a cookie")
+            .Produces<AuthResult>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status404NotFound)
+            .ProducesProblem(StatusCodes.Status500InternalServerError);
     }
 }
